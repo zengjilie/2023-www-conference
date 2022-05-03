c8eddf3b5dc5db5dcef0a91750729fa581c4c39c
zengjilie <zengjilie@gmail.com>
2022-05-03 02:00:28 -0500

    remove out

--- out/_next/static/li60n-LUlJK-cdmoxWtwm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/62db60c3bfa45fbc.css","static/chunks/pages/index-e0247e914b29a32c.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
